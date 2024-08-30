80bc8c205a0d8bc473a6461df771deb550e8079e
Birdie <hananefegrachi2002@gmail.com>
2024-08-30 13:14:09 +0200

    Create repeat_str.c

---
+++ codewars/repeat_str.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+char *repeat_str(size_t count, const char *src)
+{
+// allocate a string on the heap
+  char *ret = calloc(strlen(src) * (count), sizeof(char));
+  if (!ret) return NULL;
+  size_t i, a = 0;
+  while (count) {
+    i = 0;
+    while (src[i])
+      ret[a++] = src[i++];
+    count--;
+  }
+  return ret;
+}
